41b07df24e13f2e7612e2e826f996f41409b53fd
Abdul Awaul Raju <ibnealbar704@gmail.com>
2026-05-27 12:36:56 +0600

    Added BFS Implementation

---
+++ BFS/main.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['G'],
+    'F': [],
+    'G': []
+}
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    print("BFS Traversal:")
+
+    while queue:
+        node = queue.popleft()
+
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+
+            for neighbor in graph[node]:
+                queue.append(neighbor)
+
+bfs(graph, 'A')
